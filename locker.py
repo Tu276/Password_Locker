61eb699154e1a3f4acf99658e60168cd9aabeba4
Tu276 <tu276nathan@gmail.com>
2020-09-06 13:19:26 +0300

    add class credentials & variables

--- locker.py
+++ locker.py
@@ -6,6 +6,23 @@ class user:
     user_List = []
 
 
-    def __init__(self,login_username,password):
+    def __init__(self,login_username,user_password):
+
+
         self.login_username  = login_username
-        self.login_username = password
+        self.user_password = user_password
+
+
+class credentials:
+    """
+    class that generates new credentials 
+    """
+    
+    credentials_List = []
+
+
+    def __init__ (self,account_name,account_username,account_password):
+
+        self.account_name = account_name 
+        self.account_username = account_username
+        self.account_password = account_password
