ee2c9e61c50ddda72dadf21115c49629944256e9
Tu276 <tu276nathan@gmail.com>
2020-09-06 12:45:22 +0300

    ad class user , & variables

---
+++ locker.py
@@ -0,0 +1,11 @@
+class user:
+    """
+    class that generates new users
+    """
+
+    user_List = []
+
+
+    def __init__(self,login_username,password):
+        self.login_username  = login_username
+        self.login_username = password
