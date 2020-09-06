f868c0d85b3397e20026832ef42ef676e367274e
Tu276 <tu276nathan@gmail.com>
2020-09-06 23:50:19 +0300

    add create new user

---
+++ run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.8
+from locker import User
+from locker import Credentials
+
+def create_user(luname,upassword):
+    '''
+    function create new user
+    '''
+    new_user = User(luname,upassword)
+
+    return new user
